e95cfe68881e81fd9133365d4905589f35586ae0
Sean Bethard <bethardsean@gmail.com>
2017-06-23 17:14:27 -0700

    friends ids

--- get_neighbors.py
+++ get_neighbors.py
@@ -23,6 +23,11 @@ import tweepy
 import argparse
 import csv
 import random
+import logging
+import time
+
+logging.basicConfig(filename='get_neighbors.log', filemode='w', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 consumer_key = 'XXXX'
 consumer_secret = 'XXXX'
@@ -41,23 +46,39 @@ def get_site_handle_pairs(fname):
             l = list(reader)
     return [(pair[0], pair[1]) for pair in l if pair]
 
-def get_audiences(handles):
+
+def get_brands_followers(handles):
     result = []
     for handle in handles:
         ids = api.followers_ids(handle)
         random_ids = []
-        for i in range(120):
-            random_ids.append(random.choice(ids))
+        while len(random_ids) != 120:
+            choice = random.choice(ids)
+            if choice not in random_ids:
+                random_ids.append(choice)
         result.append((handle, random_ids))
     return result
+
+
+def get_followers_friends(brands_followers):
+    followers_friends = []
+    for brand, followers in brands_followers:
+        for id in followers:
+            friends_ids = []
+            try:
+                for friend_id in tweepy.Cursor(api.friends_ids, id=id).items(5000):
+                    friends_ids.extend([friend_id])
+                    time.sleep(60)
+            except tweepy.error.TweepError:
+                logger.debug('could not get friends of user %s' % id)
+        followers_friends.append((id, friends_ids))
+    return followers_friends
+
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("sites", help='path to site list')
     args = argparser.parse_args()
     handles = [pair[1] for pair in get_site_handle_pairs(args.sites)]
-
-    for brand, audience in get_audiences(handles):
-        print brand, audience
-        print len(audience)
-        print
+    brands_followers = get_brands_followers(handles)
+    followers_friends = get_followers_friends(brands_followers)
