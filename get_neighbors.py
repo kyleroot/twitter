6f3829509be8fdacce8b96a5dd31e7b2b0dc545d
Kyle Root <kyle@lolatravel.com>
2017-08-02 19:10:01 -0400

    Retrieve secret values from env vars

--- get_neighbors.py
+++ get_neighbors.py
@@ -25,14 +25,15 @@ import csv
 import random
 import logging
 import time
+import os
 
 logging.basicConfig(filename='get_neighbors.log', filemode='w', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-consumer_key = 'XXXX'
-consumer_secret = 'XXXX'
-access_token = 'XXXX'
-access_token_secret = 'XXXX'
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
